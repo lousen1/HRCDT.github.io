fb0f46759b781f97c63fd403b2132f6dc14f31aa
lousen1 <146497033+lousen1@users.noreply.github.com>
2026-08-06 10:12:00 +0800

    Refresh PWA cache for care corrections

--- plant-water-reminder/sw.js
+++ plant-water-reminder/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jiaojiaohao-v4";
+const CACHE = "jiaojiaohao-v5";
 const ASSETS = ["./", "./styles.css", "./family-sync.css", "./app.js", "./manifest.webmanifest"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
